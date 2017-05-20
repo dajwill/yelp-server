2e2d3ff695e359aa08a35de618d62779923d5eef
David <dajwilliam@gmail.com>
2017-05-20 18:25:32 -0400

    init commit

---
+++ server/index.js
@@ -0,0 +1,41 @@
+var express = require('express')
+var cors = require('cors')
+const axios = require('axios');
+var app = express()
+
+app.use(cors())
+app.enable('trust proxy')
+
+app.get('/', (req, res) => {
+  return res.send('Hello')
+})
+
+app.get('/search', (req, res) => {
+  var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  console.log(req.ip);
+  console.log(req.ips);
+  console.log(ip);
+  // let { filter, query } = req.query
+  let filter = req.query.filter || ''
+  let query = req.query.query || ''
+  let url = `https://api.yelp.com/v3/businesses/search`
+  let headers = {
+    Authorization: process.env.YELP_AUTH
+  }
+  let params = {
+    location: 48103,
+    term: query,
+    categories: filter
+  }
+
+  axios.get(url, {params: params, headers: headers})
+    .then((data) => {
+      var businesses = data.data.businesses
+      return res.send(businesses)
+    })
+    .catch((err) => res.send(new Error(err)))
+})
+
+app.listen(8081, function () {
+  console.log('CORS-enabled web server listening on port 8081')
+})
